4ccf61821262b10d0319b635a0eabb7e140528a1
Julian Buechel <jbu@zurich.ibm.com>
2021-11-22 16:06:36 +0100

    Remove links to simulator

--- Architectures/cifar_resnet.py
+++ Architectures/cifar_resnet.py
@@ -30,17 +30,16 @@ author, Yerlan Idelbayev.
 import torch
 import torch.nn.functional as F
 import torch.nn.init as init
-import ais.nn as nn
 
 __all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
 
 def _weights_init(m):
     classname = m.__class__.__name__
     #print(classname)
-    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+    if isinstance(m, torch.nn.Linear) or isinstance(m, torch.nn.Conv2d):
         init.kaiming_normal_(m.weight)
 
-class LambdaLayer(nn.AISBase):
+class LambdaLayer(torch.nn.Module):
     def __init__(self, lambd):
         super(LambdaLayer, self).__init__()
         self.lambd = lambd
@@ -49,17 +48,17 @@ class LambdaLayer(nn.AISBase):
         return self.lambd(x)
 
 
-class BasicBlock(nn.AISBase):
+class BasicBlock(torch.nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1, option='A'):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = torch.nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = torch.nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = torch.nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = torch.nn.BatchNorm2d(planes)
 
-        self.shortcut = nn.Sequential()
+        self.shortcut = torch.nn.Sequential()
         if stride != 1 or in_planes != planes:
             if option == 'A':
                 """
@@ -68,8 +67,8 @@ class BasicBlock(nn.AISBase):
                 self.shortcut = LambdaLayer(lambda x:
                                             F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
             elif option == 'B':
-                self.shortcut = nn.Sequential(
-                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                self.shortcut = torch.nn.Sequential(
+                     torch.nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
                      torch.nn.BatchNorm2d(self.expansion * planes)
                 )
 
@@ -81,17 +80,17 @@ class BasicBlock(nn.AISBase):
         return out
 
 
-class ResNet(nn.AISNet):
+class ResNet(torch.nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 16
 
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = torch.nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = torch.nn.BatchNorm2d(16)
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
-        self.linear = nn.Linear(64, num_classes)
+        self.linear = torch.nn.Linear(64, num_classes)
 
         self.apply(_weights_init)
 
@@ -102,7 +101,7 @@ class ResNet(nn.AISNet):
             layers.append(block(self.in_planes, planes, stride))
             self.in_planes = planes * block.expansion
 
-        return nn.Sequential(*layers)
+        return torch.nn.Sequential(*layers)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
@@ -112,44 +111,6 @@ class ResNet(nn.AISNet):
         out = F.avg_pool2d(out, out.size()[3])
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        return out
-
-
-
-class resnet9(nn.AISNet):
-    """
-    From https://github.com/matthias-wright/cifar10-resnet/
-    """
-    def __init__(self):
-        super(resnet9, self).__init__()
-
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False),
-            torch.nn.BatchNorm2d(num_features=16, momentum=0.9),
-            torch.nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False),
-            torch.nn.BatchNorm2d(num_features=16, momentum=0.9),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            BasicBlock(in_planes=16, planes=16, stride=1),
-            nn.Conv2d(in_channels=16, out_channels=28, kernel_size=3, stride=1, padding=1, bias=False),
-            torch.nn.BatchNorm2d(num_features=28, momentum=0.9),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=28, out_channels=56, kernel_size=3, stride=1, padding=1, bias=False),
-            torch.nn.BatchNorm2d(num_features=56, momentum=0.9),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            BasicBlock(in_planes=56, planes=56, stride=1),
-            torch.nn.MaxPool2d(kernel_size=4, stride=4),
-        )
-
-        self.fc = nn.Linear(in_features=56, out_features=10, bias=True)
-
-    def forward(self, x):
-        out = self.conv(x)
-        out = out.view(-1, out.shape[1] * out.shape[2] * out.shape[3])
-        out = self.fc(out)
         return out
 
 
