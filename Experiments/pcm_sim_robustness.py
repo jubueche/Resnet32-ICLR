b4cba0550533c3718f0c87cc2bcfe37aaaf6e0d4
Julian Buechel <jbu@zurich.ibm.com>
2021-11-26 08:05:07 +0100

    Add plotting pcm AWP

--- Experiments/pcm_sim_robustness.py
+++ Experiments/pcm_sim_robustness.py
@@ -18,6 +18,8 @@ seeds = [0,1]
 eta_trains = [0.11]
 beta_robustnesses = [0.01,0.025,0.05,0.1]
 attack_size_mismatches = [0.01,0.025,0.05,0.1]
+gamma = 0.1
+eps_pgas = [0.0,0.001,0.01]
 mode = "range"
 base_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -25,6 +27,12 @@ class pcm_sim_robustness:
 
     @staticmethod
     def train_grid():
+        resnet_awp = [Resnet.make()]
+        resnet_awp = configure(resnet_awp, {"n_attack_steps":3, "batch_size":256, "clipping_alpha":2.0,
+                                    "start_epoch":60, "pretrained":True, "epochs":200, "gamma":gamma})
+        resnet_awp = split(resnet_awp, "eps_pga", eps_pgas)
+        resnet_awp = split(resnet_awp, "attack_size_mismatch", attack_size_mismatches)
+
         resnet = [Resnet.make()]
         resnet = configure(resnet, {"eta_mode":mode, "n_attack_steps":3,
                                     "batch_size":256, "clipping_alpha":2.0, "start_epoch":60,
@@ -39,14 +47,14 @@ class pcm_sim_robustness:
                                     "epochs":200})
         baseline = split(baseline, "eta_train", eta_trains)
         baseline = baseline + fp_baseline.train_grid()
-        resnet = split(resnet + baseline, "seed", seeds)
+        resnet = split(resnet + baseline + resnet_awp, "seed", seeds)
         return resnet
 
     @staticmethod
     def visualize():
         grid = pcm_sim_robustness.train_grid()
         grid = run(grid, "train", run_mode="load", store_key="*")("{*}")
-        grid = configure(grid, {"mode":"bsub"})
+        grid = configure(grid, {"mode":"direct"})
         times = [25.0, 3600.0, 86400.0, 2592000.0, 31536000.0]
         N_rep = 25
         grid = run(
@@ -67,10 +75,23 @@ class pcm_sim_robustness:
             ax.spines['right'].set_visible(False)
             ax.spines['top'].set_visible(False)
 
-        def plot_errorbar(ax, times, eta_train, color, grid, eta_mode, beta_robustness, attack_size_mismatch, label=None):
+        def plot_errorbar(
+            ax,
+            times,
+            eta_train,
+            color,
+            grid,
+            eta_mode,
+            beta_robustness,
+            attack_size_mismatch,
+            gamma,
+            eps_pga,
+            label=None
+        ):
             mean_acc_over_time, std_acc_over_time = query(grid, "pcm_acc_over_time",
                 where={"eta_mode":eta_mode, "eta_train":eta_train,
                        "beta_robustness":beta_robustness,
+                       "gamma":gamma, "eps_pga":eps_pga,
                        "attack_size_mismatch":attack_size_mismatch})[0]
 
             ax.fill_between(
@@ -83,8 +104,10 @@ class pcm_sim_robustness:
             )
             ax.plot(times,100*mean_acc_over_time, color=color)
 
-        baseline_test_acc = np.max(np.mean(np.array(query(grid, "test_acc", where={"eta_train":0.0})),axis=0))
-        green_vals = np.linspace(242. / 255., 0.3 ,num=len(beta_robustnesses))
+        baseline_test_acc = np.max(np.mean(np.array(query(grid, "test_acc", where={"eta_train":0.0, "gamma":0.0})),axis=0))
+        blue_vals = np.linspace(242. / 255., 0.3 ,num=len(beta_robustnesses))
+        eps_colors = np.linspace(242. / 255., 0.3 ,num=len(eps_pgas))
+
         for i,eta_train in enumerate(eta_trains):
             for j,attack_size_mismatch in enumerate(attack_size_mismatches):
                 ax = axes[i,j]
@@ -96,7 +119,7 @@ class pcm_sim_robustness:
                 ax.set_xlabel(r"$T_{inf}$ (s)")
                 ax.tick_params(axis='both', which='major', labelsize=6)
                 for k,beta_robustness in enumerate(beta_robustnesses):
-                    color = (0.,green_vals[k],1.)
+                    color = (0.3,blue_vals[k],1.)
                     plot_errorbar(
                         ax=ax,
                         times=times,
@@ -105,8 +128,28 @@ class pcm_sim_robustness:
                         grid=grid,
                         eta_mode=mode,
                         beta_robustness=beta_robustness,
-                        attack_size_mismatch=attack_size_mismatch
+                        attack_size_mismatch=attack_size_mismatch,
+                        gamma=0.0,
+                        eps_pga=0.0
                     )
+                # - Plot AWP
+                for k,eps_pga in enumerate(eps_pgas):
+                    color = (0.0,eps_colors[k],0.01)
+                    plot_errorbar(
+                        ax=ax,
+                        times=times,
+                        eta_train=0.0,
+                        color=color,
+                        grid=grid,
+                        eta_mode="ind",
+                        beta_robustness=0.0,
+                        attack_size_mismatch=attack_size_mismatch,
+                        gamma=gamma,
+                        eps_pga=eps_pga,
+                        label=None if k > 0 else r"AWP"
+                    )
+
+
                 plot_errorbar(
                         ax=ax,
                         times=times,
@@ -116,19 +159,30 @@ class pcm_sim_robustness:
                         eta_mode=mode,
                         beta_robustness=0.0,
                         attack_size_mismatch=0.2,
+                        gamma=0.0,
+                        eps_pga=0.0,
                         label=r"$\beta_{rob}=0.0,\eta_{train}=$"+("%.3f"%eta_train)
                     )
                 if j == 0:
                     ax.legend()
 
             norm = mpl.colors.Normalize(vmin=beta_robustnesses[0],vmax=beta_robustnesses[-1])
-            cmap = ListedColormap([(0.3,g,1.0) for g in green_vals])
+            cmap = ListedColormap([(0.3,g,1.0) for g in blue_vals])
             cbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax, aspect=30)
             cbar.ax.tick_params(labelsize=6)
             cbar.outline.set_visible(False)
             cbar.set_label(r"$\beta_{rob}$")
             cbar.set_ticks(np.linspace(beta_robustnesses[0],beta_robustnesses[-1],len(beta_robustnesses)))
             cbar.set_ticklabels([str(br) for br in beta_robustnesses])
+
+            norm_awp = mpl.colors.Normalize(vmin=eps_pgas[0],vmax=eps_pgas[-1])
+            cmap_awp = ListedColormap([(0.0,g,0.01) for g in eps_colors])
+            cbar_awp = fig.colorbar(cm.ScalarMappable(norm=norm_awp, cmap=cmap_awp), ax=axes[i,0], aspect=30, location="left")
+            cbar_awp.ax.tick_params(labelsize=6)
+            cbar_awp.outline.set_visible(False)
+            cbar_awp.set_label(r"$\epsilon_{pga}$")
+            cbar_awp.set_ticks(np.linspace(eps_pgas[0],eps_pgas[-1],len(eps_pgas)))
+            cbar_awp.set_ticklabels([str(ep) for ep in eps_pgas])
 
         figures_path = os.path.join(base_path,"../Resources/Figures")
         if not os.path.exists(figures_path):
